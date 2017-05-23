0e0c97b3805c7cc20d9b7e318b2e5254901b0124
Quentin Joseph <qej2@icloud.com>
2017-05-23 10:22:14 -0400

    fixed routing again

--- public/js/routes.js
+++ public/js/routes.js
@@ -2,15 +2,14 @@ var app=angular.module('ourApp', ['ngRoute'])
 app.config(function($routeProvider) {
     $routeProvider.when('/view1', {
       templateUrl: 'views/view1.html',
-      
+
     })
     .when('/view2', {
       templateUrl: 'views/view2.html',
-      controller: myController
+
 
     }).when('/view3', {
       templateUrl: 'views/view3.html',
-      controller: myController
 
     })
     .otherwise('/view1');
