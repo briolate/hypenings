0be5d7b1991a6bd261cc92c8b98cf764e9422514
hunter <hskempton@gmail.com>
2017-05-24 10:37:55 -0400

    changed spelling

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-var pool = require("./pg­connection­pool");
+var pool = require("./pg­-connection­-pool");
 
 function errorCallback(res) {
     return function(err) {
