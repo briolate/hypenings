e64442e3e57dbe69d1a05931867a550e6bd59c74
Quentin Joseph <qej2@icloud.com>
2017-05-26 13:15:46 -0400

    database functioning

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ function errorCallback(res) {
 app.post('/events', function(req, res) {
 
     var event = req.body;
-    var sql = "INSERT INTO events(userName, eventName, date, description, hood, lat, lng) VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, $6::decimal, $7::decimal)";
-    var values = [event.user, event.eventName, event.date, event.description, event.hood, event.lat, event.long];
+    var sql = "INSERT INTO events(userName, eventName, date, description, hood, pic, lat, lng) VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, $6::text,$7::decimal, $8::decimal)";
+    var values = [event.user, event.eventName, event.date, event.description, event.hood, event.pic, event.lat, event.long];
 
     pool.query(sql, values).then(function() {
         res.status(201);
@@ -51,7 +51,7 @@ app.get('/localevents', function(req, res) {
     var lat = req.query.lat;
     var lng = req.query.lng;
     var values = [lat, lng]
-    //this sql variable selects from the data base events that are within our given radius and also within our expiration perameters.  
+    //this sql variable selects from the data base events that are within our given radius and also within our expiration perameters.
     var sql = "select * from (SELECT * , (3959 * acos (cos ( radians($1::real) )* cos( radians( lat ) )* cos( radians( lng )- radians($2::real) )+ sin ( radians($1::real) )* sin( radians( lat ) ))) AS distance FROM events)AS distance where distance < 1 AND timeadded + INTERVAL '48 HOUR' > now()";
 
     console.log("lat = " + lat);
@@ -85,7 +85,8 @@ description VARCHAR(200),
 hood VARCHAR(40),
 pic VARCHAR(40),
 lat DECIMAL(11,8),
-lng DECIMAL(10,8)
+lng DECIMAL(10,8),
+timeadded TIMESTAMP DEFAULT NOW()
 );
 
 */
