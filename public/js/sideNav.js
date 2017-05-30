c1387f3f81fe98074206a925a04044031972b067
Evan Briolat <briolate@gmail.com>
2017-05-30 11:07:07 -0400

    New navbar

---
+++ public/js/sideNav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "140px";
+    document.getElementById("wrap").style.marginLeft = "140px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("wrap").style.marginLeft= "0";
+}
